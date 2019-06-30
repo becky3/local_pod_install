629093962265edd0d72dd1350297febd096e9d48
becky <>
2019-06-30 13:58:23 +0900

    - 初期プロジェクト追加

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'podTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for podTest
+  pod 'SVProgressHUD', :path => './mypods/SVProgressHUD/'
+
+end
---
+++ pod-local-install-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pod-local-install-test
+//
+//  Created by rhythmcansoft on 2019/06/30.
+//  Copyright © 2019 becky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
